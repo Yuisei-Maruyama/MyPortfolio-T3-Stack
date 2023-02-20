c7d8c38efa47b2be65bd8a29c7c78bbeecfd2c90
Yuisei-Maruyama <yuisei.metro@gmail.com>
2023-02-21 01:43:59 +0900

    update: RecoilRootの追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,20 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import { type AppType } from 'next/app'
+import { type Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
+import { RecoilRoot } from 'recoil'
 
-import { api } from "../utils/api";
+import { api } from '../utils/api'
 
-import "../styles/globals.css";
+import '../styles/globals.css'
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </SessionProvider>
-  );
-};
+  )
+}
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(MyApp)
