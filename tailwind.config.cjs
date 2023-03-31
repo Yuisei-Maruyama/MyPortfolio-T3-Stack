71413addcd2e83ec060f1e528d3a93e7f8e7909a
Yuisei-Maruyama <yuisei.metro@gmail.com>
2023-04-01 01:45:09 +0900

    update: color定義の追加

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      green: {
+        one: '#E7F3EF',
+        two: '#DDEEE9',
+        three: '#C6E2D9',
+        four: '#8CC5B3',
+        five: '#39705E',
+      },
+      gray: {
+        one: '#E5E7EB',
+        two: '#D1D5DB',
+        three: '#4B5563',
+        four: '#374151',
+        five: '#111827',
+      },
+      olive: {
+        one: '#E1EAE2',
+        two: '#DAE5DB',
+        three: '#A5C0A7',
+        four: '#729C76',
+      },
+      cream: {
+        one: '#FFFFFF',
+        two: '#FEFEFB',
+        three: '#FDFCF7',
+        four: '#FCFBF4',
+      },
+      steel: {
+        one: '#FAFAFA',
+        two: '#F5F5F5',
+        three: '#737373',
+      },
+      leaf: {
+        one: '#E0E7A1',
+      },
+    },
     screens: {
       // sm: "480px",
       // md: "768px",
@@ -34,4 +70,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
